229eeb119c0ee259475b88340fff79468f3a0403
kellyselden <kellyselden@gmail.com>
2017-02-01 17:09:10 -0800

    testing array fixes

--- addon/array/filter-by.js
+++ addon/array/filter-by.js
@@ -1,10 +1,23 @@
-import { normalizeArray } from './-utils';
+import computed from 'ember-computed';
+import { assert } from 'ember-metal/utils';
+import flattenKeys from 'ember-macro-helpers/flatten-keys';
+import getValue from 'ember-macro-helpers/get-value';
 
 const defaultValue = [];
 
-export default normalizeArray({ defaultValue }, (array, key, value) => {
-  if (!key) {
-    return defaultValue;
+export default function(array, key, value) {
+  assert('The object key must be a string to properly watch the array for value changes.', typeof key === 'string');
+
+  let arrayKey = array;
+  if (typeof array === 'string') {
+    arrayKey += `.@each.${key}`;
   }
-  return array.filterBy(key, value);
-});
+
+  return computed(...flattenKeys([arrayKey, value]), function() {
+    let arrayValue = getValue(this, array);
+    if (!arrayValue) {
+      return defaultValue;
+    }
+    return arrayValue.filterBy(key, getValue(this, value));
+  }).readOnly();
+}
--- addon/array/filter.js
+++ addon/array/filter.js
@@ -1,5 +1,25 @@
-import { normalizeArray } from './-utils';
+import computed from 'ember-computed';
+import flattenKeys from 'ember-macro-helpers/flatten-keys';
+import getValue from 'ember-macro-helpers/get-value';
 
-export default normalizeArray({ defaultValue: [] }, (array, callback) => {
-  return array.filter(callback);
-});
+const defaultValue = [];
+
+export default function(array, callback) {
+  let arrayKey = array;
+  if (typeof array === 'string') {
+    let i = array.indexOf('.@each');
+    if (i !== -1) {
+      array = array.substr(0, i);
+    } else {
+      arrayKey += `.[]`;
+    }
+  }
+
+  return computed(...flattenKeys([arrayKey]), function() {
+    let arrayValue = getValue(this, array);
+    if (!arrayValue) {
+      return defaultValue;
+    }
+    return arrayValue.filter(callback);
+  }).readOnly();
+}
